862fff7793a67fd5ed27cf72d672ea3b86123f97
RAFH82 <hrechkar@gmail.com>
2020-11-18 18:39:48 +0000

    Created file, and markup html to append to index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,51 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(() => {
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac",
+    },
+    "content": {
+      "text":
+        "If I have seen further it is by standing on the shoulders of giants",
+    },
+    "created_at": 1461116232227,
+  };
 
+  function createTweetElement(tweetData) {
+    const markup = `
+    <article class="tweet">
+          <header class="tweet-header active">
+            <div class="avatar-container">
+              <div>
+                <img class="avatar-img active" src="${tweetData.user.avatars}" />
+              </div>
+              <div class="avatar-name active">${tweetData.user.name}</div>
+            </div>
+            <div class="avatar-handle">${tweetData.user.handle}</div>
+          </header>
+          <div class="tweet-body-container">
+            <div class="tweet-body active">${tweetData.content.text}</div>
+          </div>
+          <footer class="tweet-footer active">
+            <div>${tweetData.created_at}</div>
+            <div>
+              <i class="fa fa-flag" aria-hidden="true"></i>
+              <i class="fa fa-heart" aria-hidden="true"></i>
+              <i class="fa fa-retweet" aria-hidden="true"></i>
+            </div>
+          </footer>
+        </article>
+        `;
+    return markup;
+  }
+
+  const $tweet = createTweetElement(tweetData);
+
+  console.log($tweet);
+
+  $("#tweets-container").append($tweet);
+});
