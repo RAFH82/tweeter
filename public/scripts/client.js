0e6bde5786d79f6368e22cefbe38d4b96eb055cb
RAFH82 <hrechkar@gmail.com>
2020-11-18 20:44:46 +0000

    Created new renderTweets fnc to handle tweets obj

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,21 +4,34 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(() => {
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac",
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac",
+      },
+      "content": {
+        "text":
+          "If I have seen further it is by standing on the shoulders of giants",
+      },
+      "created_at": 1461116232227,
     },
-    "content": {
-      "text":
-        "If I have seen further it is by standing on the shoulders of giants",
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd",
+      },
+      "content": {
+        "text": "Je pense , donc je suis",
+      },
+      "created_at": 1461113959088,
     },
-    "created_at": 1461116232227,
-  };
+  ];
 
   function createTweetElement(tweetData) {
-    const markup = `
+    const $tweet = `
     <article class="tweet">
           <header class="tweet-header active">
             <div class="avatar-container">
@@ -42,12 +55,18 @@ $(document).ready(() => {
           </footer>
         </article>
         `;
-    return markup;
+    return $tweet;
   }
 
-  const $tweet = createTweetElement(tweetData);
+  function renderTweets(tweets) {
+    for (let tweetsData of tweets) {
+      const $tweet = createTweetElement(tweetsData);
+      $("#tweets-container").append($tweet);
+    }
+  }
 
-  console.log($tweet);
+  renderTweets(data);
+  // const $tweet = createTweetElement(tweetData);
 
-  $("#tweets-container").append($tweet);
+  // $("#tweets-container").append($tweet);
 });
