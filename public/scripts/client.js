3fb39fa90af16d2cf3fe4542977e6299d802ac11
RAFH82 <hrechkar@gmail.com>
2020-11-18 23:22:27 +0000

    Created AJAX post and GET requests. Created loadTweets fnc

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(() => {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac",
-      },
-      "content": {
-        "text":
-          "If I have seen further it is by standing on the shoulders of giants",
-      },
-      "created_at": 1461116232227,
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd",
-      },
-      "content": {
-        "text": "Je pense , donc je suis",
-      },
-      "created_at": 1461113959088,
-    },
-  ];
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png",
+  //       "handle": "@SirIsaac",
+  //     },
+  //     "content": {
+  //       "text":
+  //         "If I have seen further it is by standing on the shoulders of giants",
+  //     },
+  //     "created_at": 1461116232227,
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd",
+  //     },
+  //     "content": {
+  //       "text": "Je pense , donc je suis",
+  //     },
+  //     "created_at": 1461113959088,
+  //   },
+  // ];
 
   function createTweetElement(tweetData) {
     const dayInMs = 1000 * 60 * 60 * 24;
@@ -71,5 +71,26 @@ $(document).ready(() => {
     }
   }
 
-  renderTweets(data);
+  function loadTweets() {
+    $.ajax("/tweets", { type: "GET" }).then(data => {
+      renderTweets(data);
+    });
+  }
+
+  loadTweets();
+
+  $(".new-tweet form").submit(event => {
+    event.preventDefault();
+    const $formData = $("#tweet-text").serialize();
+    $.ajax("/tweets", {
+      type: "POST",
+      data: $formData,
+    })
+      .then($("#tweet-text").val(""))
+      .catch(error => {
+        console.log(error);
+      });
+  });
+
+  // renderTweets(data);
 });
