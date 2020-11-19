ad8da1cae127c9d5934a80b77429f921717b4b11
RAFH82 <hrechkar@gmail.com>
2020-11-19 03:32:53 +0000

    Added jquery err logic to slide error away. line 7

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   const count = 140;
 
   $textArea.on("input", function () {
+    $(".error-line").slideUp();
     $output.val(count - $textArea.val().length);
     if (count - $textArea.val().length < 0) {
       $output.addClass("counterFull");
