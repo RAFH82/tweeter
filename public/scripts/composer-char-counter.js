8a71d246ee343c7b6155eb209c0d1f6b5263d558
RAFH82 <hrechkar@gmail.com>
2020-11-23 19:44:50 +0000

    Updated func to use  variable

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,11 +2,12 @@ $(document).ready(function () {
   const $textArea = $("textarea");
   const $output = $("output");
   const count = 140;
-
+  let $tweetLength;
   $textArea.on("input", function () {
+    $tweetLength = count - $textArea.val().length;
     $(".error-line").slideUp();
-    $output.val(count - $textArea.val().length);
-    if (count - $textArea.val().length < 0) {
+    $output.val($tweetLength);
+    if ($tweetLength < 0) {
       $output.addClass("counterFull");
     } else {
       $output.removeClass("counterFull");
